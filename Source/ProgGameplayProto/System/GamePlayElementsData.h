e13dc4439fa9f10925fec7c4a90b0189108ad8c0
Clemaah <66416477+Clemaah@users.noreply.github.com>
2023-12-13 10:53:43 +0100

    Resolving github conflits...

---
+++ Source/ProgGameplayProto/System/GamePlayElementsData.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GamePlayElementsData.generated.h"
+
+class UCharacterData;
+class UWeaponData;
+class UProjectileData;
+/**
+ *
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API UGamePlayElementsData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+	TArray<UCharacterData*> Characters;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+	TArray<UWeaponData*> Weapons;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Parameters")
+	TArray<UProjectileData*> Projectiles;
+};
